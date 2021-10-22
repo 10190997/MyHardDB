5a121598d1369608d2feb7188530833bed1135b6
10190997 <petrocollege10190997@gmail.com>
2021-10-22 13:21:32 +0300

    фильтр

--- app/src/main/java/com/example/myharddb/MainActivity.java
+++ app/src/main/java/com/example/myharddb/MainActivity.java
@@ -6,11 +6,15 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText ageInput;
     private ListView usersList;
     private SimpleCursorAdapter usersAdapter;
+    private EditText filterInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,23 @@ public class MainActivity extends AppCompatActivity {
         nameInput = findViewById(R.id.etName);
         ageInput = findViewById(R.id.etAge);
         usersList = findViewById(R.id.lvData);
+        filterInput = findViewById(R.id.etFilter);
+        filterInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                GetFilteredData(s.toString());
+            }
+        });
         helper = new DBHelper(this);
     }
 
@@ -46,23 +68,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showData(View view) {
-        SQLiteDatabase db = helper.getWritableDatabase();
-        Cursor query = db.query(DBHelper.TABLE_USERS,
-                null,
-                DBHelper.KEY_NAME + " like ?",
-                new String[]{"%max%"},
-                null,
-                null,
-                null);
-        // usersAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item, query,
-        //        new String[]{DBHelper.KEY_NAME, DBHelper.KEY_AGE}, new int[] {android.R.id.text1, android.R.id.text2}, 0);
-        usersAdapter = new SimpleCursorAdapter(this, R.layout.user_item, query,
-                new String[]{DBHelper.KEY_ID, DBHelper.KEY_NAME, DBHelper.KEY_AGE}, new int[] {R.id.user_id, R.id.user_name, R.id.user_age}, 0);
-        usersList.setAdapter(usersAdapter);
+        GetFilteredData("");
     }
 
     public void deleteData(View view) {
         SQLiteDatabase db = helper.getWritableDatabase();
         db.delete(DBHelper.TABLE_USERS, null, null);
+    }
+
+    public void GetFilteredData(String filter) {
+            SQLiteDatabase db = helper.getWritableDatabase();
+            Cursor query = db.query(DBHelper.TABLE_USERS,
+                    null,
+                    DBHelper.KEY_NAME + " like ?",
+                    new String[]{"%" + filter + "%"},
+                    null,
+                    null,
+                    null);
+            usersAdapter = new SimpleCursorAdapter(this, R.layout.user_item, query,
+                    new String[]{DBHelper.KEY_ID, DBHelper.KEY_NAME, DBHelper.KEY_AGE}, new int[] {R.id.user_id, R.id.user_name, R.id.user_age}, 0);
+            usersList.setAdapter(usersAdapter);
     }
 }
